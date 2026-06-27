c0929e333434b3ce4d5a57d7ab5da1fed3f44f58
zhiying <zhiying15@hotmail.com>
2026-06-27 16:43:42 +0800

    renames

--- backend/src/main/kotlin/FinanceTrackerApplication.kt
+++ backend/src/main/kotlin/FinanceTrackerApplication.kt
@@ -2,6 +2,7 @@ package com.finance
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
